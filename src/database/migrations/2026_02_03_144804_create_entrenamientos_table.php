95b950eef30946d7b526a6b10690a46ec6163773
Mark Iza <markantoniiza@gmail.com>
2026-02-05 22:09:43 +0100

    entrenamiento

--- src/database/migrations/2026_02_03_144804_create_entrenamientos_table.php
+++ src/database/migrations/2026_02_03_144804_create_entrenamientos_table.php
@@ -11,9 +11,47 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('entrenamientos', function (Blueprint $table) {
+        Schema::create('entrenamiento', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            //claves foraneas
+            $table->unsignedInteger('id_ciclista')->nullable(false);
+            $table->unsignedInteger('id_bicicleta')->nullable(false);
+            $table->unsignedInteger('id_sesion')->nullable(true);
+
+
+            $table->dateTime('fecha')->nullable(false);
+            $table->time('duracion')->nullable(false);
+            $table->decimal('kilometros', 6,2)->nullable(false);
+            $table->string('recorrido',150)->nullable(false);
+
+            $table->integer('pulso_medio')->nullable();
+            $table->integer('pulso_max')->nullable();
+            $table->integer('potencia_media')->nullable();
+            $table->integer('potencia_normalizada')->nullable(false);
+            $table->decimal('velocidad_media', 5, 2)->nullable(false);
+            $table->decimal('puntos_estres_tss', 6, 2)->nullable();
+            $table->decimal('factor_intensidad_if', 4, 3)->nullable();
+            $table->integer('ascenso_metros')->nullable();
+            $table->string('comentario', 255)->nullable();
+
+            $table->foreign('id_ciclista')
+                  ->references('id')
+                  ->on('ciclista')
+                  ->onDelete('restrict')
+                  ->onUpdate('cascade');
+
+            $table->foreign('id_bicicleta')
+                  ->references('id')
+                  ->on('bicicleta')
+                  ->onDelete('restrict')
+                  ->onUpdate('cascade');
+            
+            $table->foreign('id_sesion')
+                  ->references('id')
+                  ->on('sesion_entrenamiento')
+                  ->onDelete('set null')
+                  ->onUpdate('cascade');
+
         });
     }
 
@@ -22,6 +60,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('entrenamientos');
+        Schema::dropIfExists('entrenamiento');
     }
 };
